483cd438a44a4a3bb850445ae52f81137d9c0caf
ByronSantis <byronsalga345@gmail.com>
2024-08-05 16:07:53 -0400

    1 - notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import authRoutes from "./routes/auth.rotues.js"
 import userRoutes from "./routes/user.rotues.js"
 import postRoutes from "./routes/post.rotues.js"
+import notificationRoutes from "./routes/notification.rotues.js"
 import connectMongoDB from './db/connectMongoDB.js';
 import cookieParser from 'cookie-parser';
 import {v2 as cloudinary} from "cloudinary";
@@ -29,6 +30,8 @@ console.log(process.env.MONGO_URI);
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/notifications", notificationRoutes);
+
 
 
 app.listen(PORT, () => {
